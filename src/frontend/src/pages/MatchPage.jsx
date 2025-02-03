a5403eb72cd28444fe1b6938f3a5456bfb07e50e
JKai27 <kailassargar5@gmail.com>
2025-02-03 18:13:54 +0100

    Styled the team-page

--- src/frontend/src/pages/MatchPage.jsx
+++ src/frontend/src/pages/MatchPage.jsx
@@ -1,6 +1,5 @@
 import {React, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import {MatchSmallCard} from "../components/MatchSmallCard.jsx";
 import {MatchDetailCard} from "../components/MatchDetailCard.jsx";
 
 export const MatchPage = () => {
